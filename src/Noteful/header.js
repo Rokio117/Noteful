bedba7e80095fc0af6a5463fd0a6f769e0b41305
Nick Hillary <hillan02@gmail.com>
2019-08-20 16:24:14 -0500

    added link to main page

--- src/Noteful/header.js
+++ src/Noteful/header.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
-    return <h1>Noteful</h1>;
+    return (
+      <h1>
+        <Link to="/">Noteful</Link>
+      </h1>
+    );
   }
 }
