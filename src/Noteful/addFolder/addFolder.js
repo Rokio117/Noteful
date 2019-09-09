3493ed512b649c8c5924fa6e3f138132c16d8c74
Nick Hillary <hillan02@gmail.com>
2019-09-09 14:14:09 -0500

    added history goback when submitting folder name

--- src/Noteful/addFolder/addFolder.js
+++ src/Noteful/addFolder/addFolder.js
@@ -3,11 +3,19 @@ import { withRouter } from 'react-router-dom';
 
 class addFolder extends Component {
   render() {
+    console.log(this.props.history, 'history in addFolder');
+
     return (
       <fieldset>
-        <form>
-          <label for="folderInput">Enter a Folder Name</label>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            this.props.history.goBack();
+          }}
+        >
+          <label htmlFor="folderInput">Enter a Folder Name</label>
           <input type="text" id="folderInput"></input>
+          <button type="submit">Submit</button>
         </form>
       </fieldset>
     );
